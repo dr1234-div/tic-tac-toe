ae4f8ed08e6be95423cf324362397a02bc3490be
hdr <1639425004@qq.com>
2023-08-24 10:57:08 +0800

    STYLE: 样式文件缩进调整

--- src/GoBang/index.tsx
+++ src/GoBang/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './index.css';
 import useHook from './useHook';
@@ -8,120 +8,120 @@ import useHook from './useHook';
  *@return 渲染棋盘、游戏类型的切换按钮以及历史记录的跳转按钮
  */
 const GobangHook = () => {
-  type palyArrType = {
-      row: number;
-      col: number;
-      chess: number;
-  }[]
-  const { play, setpalyArr, chessman, palyArr, setChess, setChessmMan } = useHook();
-  const navigate = useNavigate();
-  const border = Array(20).fill(null);
-  const [history, setHistory] = useState<palyArrType>([]);
+    type palyArrType = {
+        row: number;
+        col: number;
+        chess: number;
+    }[]
+    const { play, setpalyArr, chessman, palyArr, setChess, setChessmMan } = useHook();
+    const navigate = useNavigate();
+    const border = Array(20).fill(null);
+    const [history, setHistory] = useState<palyArrType>([]);
 
-  // 当前步骤状态存储
-  const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[currentMove];
+    // 当前步骤状态存储
+    const [currentMove, setCurrentMove] = useState(0);
+    const currentSquares = history[currentMove];
 
-  /**
-   * @param {number} rowIndex 棋子的水平坐标
-   * @param {number} colIndex 棋子的垂直坐标
-   * @description 该函数实现对下棋动作的回调监听，每次执行都会保存以往的历史记录和当前的索引和执行play函数
-   */
-  function playChess (rowIndex: number, colIndex: number) {
-      if (chessman === '获胜者：黑棋' || chessman === '获胜者：白棋') return;
+    /**
+     * @param {number} rowIndex 棋子的水平坐标
+     * @param {number} colIndex 棋子的垂直坐标
+     * @description 该函数实现对下棋动作的回调监听，每次执行都会保存以往的历史记录和当前的索引和执行play函数
+     */
+    function playChess (rowIndex: number, colIndex: number) {
+        if (chessman === '获胜者：黑棋' || chessman === '获胜者：白棋') return;
 
-      // 每次下棋都会更新history
-      let nextHistory;
-      if (currentSquares === palyArr[palyArr.length - 1]) {
-          nextHistory = [...history.slice(0, currentMove + 1)];
-      } else {
-          nextHistory = [...history.slice(0, currentMove + 1), palyArr[palyArr.length - 1]];
-      }
-      setHistory(nextHistory);
-      setCurrentMove(nextHistory.length - 1);
-      play(rowIndex, colIndex);
-  }
+        // 每次下棋都会更新history
+        let nextHistory;
+        if (currentSquares === palyArr[palyArr.length - 1]) {
+            nextHistory = [...history.slice(0, currentMove + 1)];
+        } else {
+            nextHistory = [...history.slice(0, currentMove + 1), palyArr[palyArr.length - 1]];
+        }
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+        play(rowIndex, colIndex);
+    }
 
-  /**
-   * @param {number} rowIndex 棋子的横坐标
-   * @param {number} colIndex 棋子的纵坐标
-   * @return {node} 返回一个节点，用于渲染黑棋或白棋
-   */
-  const teml = (rowIndex: number, colIndex: number) => {
-      if (palyArr.find((item: { row: number, col: number, chess: number }) => item.row === rowIndex && item.col === colIndex)) {
-          return palyArr.find((item: { row: number, col: number, chess: number }) => item.row === rowIndex && item.col === colIndex)?.chess === 1 ? (
-              <div className="chessboard-cell-black"></div>
-          ) : (
-              <div className="chessboard-cell-white"></div>
-          );
-      }
-      return (
-          <div
-              className="chessboard-cell-click"
-              onClick={() => playChess(rowIndex, colIndex)}
-          ></div>
-      );
-  };
+    /**
+     * @param {number} rowIndex 棋子的横坐标
+     * @param {number} colIndex 棋子的纵坐标
+     * @return {node} 返回一个节点，用于渲染黑棋或白棋
+     */
+    const teml = (rowIndex: number, colIndex: number) => {
+        if (palyArr.find((item: { row: number, col: number, chess: number }) => item.row === rowIndex && item.col === colIndex)) {
+            return palyArr.find((item: { row: number, col: number, chess: number }) => item.row === rowIndex && item.col === colIndex)?.chess === 1 ? (
+                <div className="chessboard-cell-black"></div>
+            ) : (
+                <div className="chessboard-cell-white"></div>
+            );
+        }
+        return (
+            <div
+                className="chessboard-cell-click"
+                onClick={() => playChess(rowIndex, colIndex)}
+            ></div>
+        );
+    };
 
-  // 记录棋子状态
-  const moves = history.map((__, index) => {
-      return (
-          <li key={index}>
-              <button onClick={() => jumpTo(index)}>{`跳转到步骤：${Number(index + 1)}`}</button>
-          </li>
-      );
-  });
+    // 记录棋子状态
+    const moves = history.map((__, index) => {
+        return (
+            <li key={index}>
+                <button onClick={() => jumpTo(index)}>{`跳转到步骤：${Number(index + 1)}`}</button>
+            </li>
+        );
+    });
 
-  /**
-   * @param {number} nextMove 传入指定的步骤数
-   * @return {node} 跳转到指定的棋盘状态
-   */
-  function jumpTo (nextMove: number) {
-      // 获胜后将不能进行悔棋
-      if (chessman === '获胜者：黑棋' || chessman === '获胜者：白棋') {
-          alert('注意：获胜后将无法进行悔棋！！！');
-          return;
-      }
-      setCurrentMove(nextMove);
-      const nextHistory = history.slice(0, nextMove + 1);
-      const filterArr = nextHistory.filter(value => value !== undefined);
-      const lastFilterArr = filterArr[filterArr.length - 1];
-      if (lastFilterArr.chess === 2) {
-          setChessmMan('下一位：黑棋');
-      } else {
-          setChessmMan('下一位：白棋');
-      }
-      setChess(lastFilterArr.chess);
-      setpalyArr(filterArr);
-  }
-  return (
-      <div className="chessboard-wrapper">
-          <div>
-              <h1 className='h1Style'>{chessman}</h1>
-              <div className="chessboard">
-                  {border.map((row, rowIndex) => (
-                      <div className="chessboard-row" key={`row + ${rowIndex}`}>
-                          {border.map((col, colIndex) => (
-                              <div className="chessboard-col" key={`col + ${colIndex}`}>
-                                  <div className="chessboard-cell">
-                                      {teml(rowIndex, colIndex)}
-                                  </div>
-                              </div>
-                          ))}
-                      </div>
-                  ))}
-              </div>
-          </div>
+    /**
+     * @param {number} nextMove 传入指定的步骤数
+     * @return {node} 跳转到指定的棋盘状态
+     */
+    function jumpTo (nextMove: number) {
+        // 获胜后将不能进行悔棋
+        if (chessman === '获胜者：黑棋' || chessman === '获胜者：白棋') {
+            alert('注意：获胜后将无法进行悔棋！！！');
+            return;
+        }
+        setCurrentMove(nextMove);
+        const nextHistory = history.slice(0, nextMove + 1);
+        const filterArr = nextHistory.filter(value => value !== undefined);
+        const lastFilterArr = filterArr[filterArr.length - 1];
+        if (lastFilterArr.chess === 2) {
+            setChessmMan('下一位：黑棋');
+        } else {
+            setChessmMan('下一位：白棋');
+        }
+        setChess(lastFilterArr.chess);
+        setpalyArr(filterArr);
+    }
+    return (
+        <div className="chessboard-wrapper">
+            <div>
+                <h1 className='h1Style'>{chessman}</h1>
+                <div className="chessboard">
+                    {border.map((row, rowIndex) => (
+                        <div className="chessboard-row" key={`row + ${rowIndex}`}>
+                            {border.map((col, colIndex) => (
+                                <div className="chessboard-col" key={`col + ${colIndex}`}>
+                                    <div className="chessboard-cell">
+                                        {teml(rowIndex, colIndex)}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
 
-          <div className='chessboard-right'>
-              <div>
-                  <button className='gameChange' onClick={() => navigate('/wellchess')}>井棋游戏</button>
-              </div>
-              <div className='stateContent'> <ul className="styoo">{moves}</ul></div>
-          </div>
-      </div>
+            <div className='chessboard-right'>
+                <div>
+                    <button className='gameChange' onClick={() => navigate('/wellchess')}>井棋游戏</button>
+                </div>
+                <div className='stateContent'> <ul className="styoo">{moves}</ul></div>
+            </div>
+        </div>
 
-  );
+    );
 };
 
 export default GobangHook;
--- src/GoBang/useHook.ts
+++ src/GoBang/useHook.ts
@@ -4,132 +4,154 @@ import { useState } from 'react';
  *@description 自定义hook,抽离逻辑代码，防止组件臃肿，封装了下棋的动作监听回调以及游戏获胜的判断方法
  */
 const useHook = () => {
-  type palyArrType = {
-      row: number;
-      col: number;
-      chess: number;
-  }[]
+    type palyArrType = {
+        row: number;
+        col: number;
+        chess: number;
+    }[];
 
-  // 记录已下的棋子数据，包含横纵坐标以及棋子的颜色
-  const [palyArr, setpalyArr] = useState<palyArrType>([]);
-  const [chess, setChess] = useState<number | null>(null);
+    // 记录已下的棋子数据，包含横纵坐标以及棋子的颜色
+    const [palyArr, setpalyArr] = useState<palyArrType>([]);
+    const [chess, setChess] = useState<number | null>(null);
 
-  const [chessArr] = useState(() => {
-      let arr = Array(20).fill('');
-      arr = arr.map(() => Array(20).fill(''));
-      return arr;
-  });
+    const [chessArr] = useState(() => {
+        let arr = Array(20).fill('');
+        arr = arr.map(() => Array(20).fill(''));
+        return arr;
+    });
 
-  // 存储下一个棋子的颜色
-  const [chessman, setChessmMan] = useState<string>('首位：黑棋');
+    // 存储下一个棋子的颜色
+    const [chessman, setChessmMan] = useState<string>('首位：黑棋');
 
-  /**
-   * @param {number} row 棋子横坐标
-   * @param {number} col 棋子纵坐标
-   * @description 用于判断下一步位黑棋/白棋，同步更新下一步棋子的颜色代码、
-   * 棋子活动的历史记录以及判断是否获胜
-   */
-  const play = (row: number, col: number) => {
-      if (chessman === '获胜者：黑棋' || chessman === '获胜者：白棋') return;
-      const newChess = chess === 1 ? 2 : 1;
-      const newPalyArr = [...palyArr, { row, col, chess: newChess }];
-      if (newChess === 2) {
-          setChessmMan('下一位：黑棋');
-      } else {
-          setChessmMan('下一位：白棋');
-      }
-      setChess(newChess);
-      setpalyArr(newPalyArr);
-      getWinner(newPalyArr, newChess, chessArr, row, col);
-  };
+    /**
+     * @param {number} row 棋子横坐标
+     * @param {number} col 棋子纵坐标
+     * @description 用于判断下一步位黑棋/白棋，同步更新下一步棋子的颜色代码、
+     * 棋子活动的历史记录以及判断是否获胜
+     */
+    const play = (row: number, col: number) => {
+        if (chessman === '获胜者：黑棋' || chessman === '获胜者：白棋') return;
+        const newChess = chess === 1 ? 2 : 1;
+        const newPalyArr = [...palyArr, { row, col, chess: newChess }];
+        if (newChess === 2) {
+            setChessmMan('下一位：黑棋');
+        } else {
+            setChessmMan('下一位：白棋');
+        }
+        setChess(newChess);
+        setpalyArr(newPalyArr);
+        getWinner(newPalyArr, newChess, chessArr, row, col);
+    };
 
-  /**
-   * @param {palyArrType} palyArr 当前棋盘上的棋子状态数组
-   * @param {number} chess 下一个棋子的颜色代码（1/2）
-   * @param {palyArrType[]} chessArr
-   * @param {number} row 当前棋子的横坐标
-   * @param {number} col 当前棋子的纵坐标
-   * @description 用户判断是否有一方获胜
-   */
-  const getWinner = (palyArr: palyArrType, chess: number, chessArr: palyArrType[], row: number, col: number) => {
-      palyArr.map((item) => {
-          return chessArr[item.row][item.col] = { ...item };
-      });
+    /**
+     * @param {palyArrType} palyArr 当前棋盘上的棋子状态数组
+     * @param {number} chess 下一个棋子的颜色代码（1/2）
+     * @param {palyArrType[]} chessArr
+     * @param {number} row 当前棋子的横坐标
+     * @param {number} col 当前棋子的纵坐标
+     * @description 用户判断是否有一方获胜
+     */
+    const getWinner = (
+        palyArr: palyArrType,
+        chess: number,
+        chessArr: palyArrType[],
+        row: number,
+        col: number
+    ) => {
+        palyArr.map((item) => {
+            return (chessArr[item.row][item.col] = { ...item });
+        });
 
-      // 分别对 上下，左右，左斜，右斜 方向进行判断是否产生winner
-      let colCount = 0;
+        // 分别对 上下，左右，左斜，右斜 方向进行判断是否产生winner
+        let colCount = 0;
 
-      // 上下
-      for (let colNumber = col + 1; colNumber < 20; colNumber++) {
-          if (chessArr[row][colNumber].chess !== chess) break;
-          colCount++;
-      }
-      for (let colNumber = col - 1; colNumber >= 0; colNumber--) {
-          if (chessArr[row][colNumber].chess !== chess) break;
-          colCount++;
-      }
-      if (colCount >= 4) {
-          chess === 1 ? setChessmMan('获胜者：黑棋') : setChessmMan('获胜者：白棋');
-          colCount = 0;
-          return;
-      }
+        // 上下
+        for (let colNumber = col + 1; colNumber < 20; colNumber++) {
+            if (chessArr[row][colNumber].chess !== chess) break;
+            colCount++;
+        }
+        for (let colNumber = col - 1; colNumber >= 0; colNumber--) {
+            if (chessArr[row][colNumber].chess !== chess) break;
+            colCount++;
+        }
+        if (colCount >= 4) {
+            chess === 1 ? setChessmMan('获胜者：黑棋') : setChessmMan('获胜者：白棋');
+            colCount = 0;
+            return;
+        }
 
-      // 左右
-      let rowCount = 0;
-      for (let rowNumber = row + 1; rowNumber < 20; rowNumber++) {
-          if (chessArr[rowNumber][col].chess !== chess) break;
-          rowCount++;
-      }
-      for (let rowNumber = row - 1; rowNumber >= 0; rowNumber--) {
-          if (chessArr[rowNumber][col].chess !== chess) break;
-          rowCount++;
-      }
-      if (rowCount >= 4) {
-          chess === 1 ? setChessmMan('获胜者：黑棋') : setChessmMan('获胜者：白棋');
-          rowCount = 0;
-          return;
-      }
+        // 左右
+        let rowCount = 0;
+        for (let rowNumber = row + 1; rowNumber < 20; rowNumber++) {
+            if (chessArr[rowNumber][col].chess !== chess) break;
+            rowCount++;
+        }
+        for (let rowNumber = row - 1; rowNumber >= 0; rowNumber--) {
+            if (chessArr[rowNumber][col].chess !== chess) break;
+            rowCount++;
+        }
+        if (rowCount >= 4) {
+            chess === 1 ? setChessmMan('获胜者：黑棋') : setChessmMan('获胜者：白棋');
+            rowCount = 0;
+            return;
+        }
 
-      // 左斜
-      let leftObliqueCount = 0;
-      for (let iNumber = row + 1, jNumber = col - 1; iNumber < 20 && jNumber >= 0; iNumber++, jNumber--) {
-          if (chessArr[iNumber][jNumber].chess !== chess) break;
-          leftObliqueCount++;
-      }
-      for (let iNumber = row - 1, jNumber = col + 1; iNumber >= 0 && jNumber < 20; iNumber--, jNumber++) {
-          if (chessArr[iNumber][jNumber].chess !== chess) break;
-          leftObliqueCount++;
-      }
-      if (leftObliqueCount >= 4) {
-          chess === 1 ? setChessmMan('获胜者：黑棋') : setChessmMan('获胜者：白棋');
-          leftObliqueCount = 0;
-          return;
-      }
+        // 左斜
+        let leftObliqueCount = 0;
+        for (
+            let iNumber = row + 1, jNumber = col - 1;
+            iNumber < 20 && jNumber >= 0;
+            iNumber++, jNumber--
+        ) {
+            if (chessArr[iNumber][jNumber].chess !== chess) break;
+            leftObliqueCount++;
+        }
+        for (
+            let iNumber = row - 1, jNumber = col + 1;
+            iNumber >= 0 && jNumber < 20;
+            iNumber--, jNumber++
+        ) {
+            if (chessArr[iNumber][jNumber].chess !== chess) break;
+            leftObliqueCount++;
+        }
+        if (leftObliqueCount >= 4) {
+            chess === 1 ? setChessmMan('获胜者：黑棋') : setChessmMan('获胜者：白棋');
+            leftObliqueCount = 0;
+            return;
+        }
 
-      // 右斜
-      let rightObliqueCount = 0;
-      for (let iNumber = row + 1, jNumber = col + 1; iNumber < 20 && jNumber < 20; iNumber++, jNumber++) {
-          if (chessArr[iNumber][jNumber].chess !== chess) break;
-          rightObliqueCount++;
-      }
-      for (let iNumber = row - 1, jNumber = col - 1; iNumber >= 0 && jNumber >= 0; iNumber--, jNumber--) {
-          if (chessArr[iNumber][jNumber].chess !== chess) break;
-          rightObliqueCount++;
-      }
-      if (rightObliqueCount >= 4) {
-          chess === 1 ? setChessmMan('获胜者：黑棋') : setChessmMan('获胜者：白棋');
-          rightObliqueCount = 0;
-          return;
-      }
-  };
+        // 右斜
+        let rightObliqueCount = 0;
+        for (
+            let iNumber = row + 1, jNumber = col + 1;
+            iNumber < 20 && jNumber < 20;
+            iNumber++, jNumber++
+        ) {
+            if (chessArr[iNumber][jNumber].chess !== chess) break;
+            rightObliqueCount++;
+        }
+        for (
+            let iNumber = row - 1, jNumber = col - 1;
+            iNumber >= 0 && jNumber >= 0;
+            iNumber--, jNumber--
+        ) {
+            if (chessArr[iNumber][jNumber].chess !== chess) break;
+            rightObliqueCount++;
+        }
+        if (rightObliqueCount >= 4) {
+            chess === 1 ? setChessmMan('获胜者：黑棋') : setChessmMan('获胜者：白棋');
+            rightObliqueCount = 0;
+            return;
+        }
+    };
 
-  return {
-      palyArr,
-      chessman,
-      setChessmMan,
-      play,
-      setpalyArr,
-      setChess,
-  };
+    return {
+        palyArr,
+        chessman,
+        setChessmMan,
+        play,
+        setpalyArr,
+        setChess,
+    };
 };
 export default useHook;
