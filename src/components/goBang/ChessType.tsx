c1b78fd24ffe31e179b07282ca204cfcae39ca33
dr1234-div <97014419+dr1234-div@users.noreply.github.com>
2023-08-28 18:10:13 +0800

    #REFACTOR: 五子棋中棋子组件命名修改，之前命名不具有语义

--- src/components/goBang/ChessType.tsx
+++ src/components/goBang/ChessType.tsx
@@ -9,10 +9,10 @@ import { ChessTypeProps } from '../../App.d';
  */
 const ChessType = (props:ChessTypeProps) => {
     const { rowIndex, colIndex, playArr, onPlay } = props;
-    const haoChess =  playArr.find((item: { row: number, col: number, chess: number }) => item.row === rowIndex && item.col === colIndex);
+    const haveChess =  playArr.find((item: { row: number, col: number, chess: number }) => item.row === rowIndex && item.col === colIndex);
     // 根据坐标判断当前位置是否已有棋子，若有根据chess来渲染黑棋或白棋，表示该区域无子，会渲染一个可点击区域，用来处理下棋逻辑
-    if (haoChess) {
-        return haoChess?.chess === 1 ? (
+    if (haveChess) {
+        return haveChess?.chess === 1 ? (
             <div className="chessboardCell-black"></div>
         ) : (
             <div className="chessboardCell-white"></div>
